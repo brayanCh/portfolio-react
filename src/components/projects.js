33b2c7cc3f5e48f612520919fe0f399ce0e187c4
Brayan S. Chavarro Leon <brayanchavarro081@gmail.com>
2021-12-16 14:13:45 -0500

    animate projects

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,20 +1,33 @@
+import {useEffect, useState} from "react";
 import exitSrc from "../media/x.png"
 
 const Project = props => {
 
-    return (
-        <div className="project">
-            <div className="header-project">
-                <div> </div>
-                <div className="head flexcenter">
-                    <h2>{props.title}</h2>
-                </div>
-                <div className="wrapper-exit" onClick={props.exit} >
-                    <img src={exitSrc} alt="exit" />
-                </div>
-            </div>
-            {props.children}
+    const [isDisplayed, setDisplay] = useState(false) 
 
+    useEffect(() => setTimeout(() => setDisplay("true"), 20) ,[])
+
+    const cerrar = () => {
+        setDisplay(false);
+        setTimeout(props.exit, 300);
+    }
+
+    return (
+        <div className={!isDisplayed ? "project hidden-pr" : "project"}>
+
+            <div className="head flexcenter">
+                <h2>{props.title}</h2>
+            </div>
+        
+            <div className="wrapper-exit" onClick={cerrar} >
+                <img src={exitSrc} alt="exit" />
+            </div>
+
+            <div className="project-content">
+
+                {props.children}
+
+            </div>
         </div>
     )
 }
--- src/pages/previous.js
+++ src/pages/previous.js
@@ -1,7 +1,12 @@
 import Card from "../components/cardwork"; 
 import { useState } from "react";
-import kosmicSrc from "../media/projects/kosmicThumbnail.png"
+import kosmicSrc from "../media/projects/kosmicThumbnail.png";
 import Project from "../components/projects";
+
+import km1 from "../media/projects/Screenshot_1_kosmic.png";
+import km2 from "../media/projects/Screenshot_2_kosmic.png";
+import km3 from "../media/projects/Screenshot_3_kosmic.png";
+
 
 
 const PreviousWork = () => {
@@ -23,6 +28,24 @@ const PreviousWork = () => {
                 prevent illness and facilitate the work of a veterinarian
                 doctor.</p>
             
+            <img src={km1} alt="starting-page" />
+
+            <p> This page was made by me and another group of wonderful
+                programmers:</p>
+        
+            <ul> 
+                <li>Juan Carlos Orozco.</li>
+                <li>Katherine Arias.</li>
+                <li>Carlos Felipe Cortes.</li>
+            </ul>
+
+            <p> It includes the register of veterinarians,
+            farm gestors, cattle and anomalies in the vital signs of 
+            a given cow.</p> 
+
+            <img src={km2} alt="data of vital signs" />
+            <img src={km3} alt="looking the data of a gestor" />
+
         </Project>
     ]
 
